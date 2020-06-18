f1a65018bf4d4616f1d5173ac42912fcfbba5e50
Surbhi Sharma <ssurbhi560@gmail.com>
2020-06-18 16:12:57 +0530

    data types

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+
+def portfolio_cost(filename):
+    '''
+    Computes the total cost of a portfolio file
+    '''
+    total_cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        header = next(f)
+        for row in f:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares*price
+        return total_cost
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,39 @@
-# report.py
-#
+
+import csv
 # Exercise 2.4
+def read_portfolio(filename):
+    ''' reads the data of file into list of tuples and dictionaries'''
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            #holding = (row[0], int(row[1]), float(row[2]))
+            stock = {
+                        'name':row[0],
+                        'shares': int(row[1]),
+                        'price': float(row[2]) 
+                    }
+            portfolio.append(stock)
+    return portfolio
+
+portfolio = read_portfolio('Data/portfolio.csv')
+print(portfolio)
+
+
+def read_prices(filename):
+    '''
+    reads the content of file in dictionaries
+    '''
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
+
+prices = read_prices('Data/prices.csv')
+print(prices)
